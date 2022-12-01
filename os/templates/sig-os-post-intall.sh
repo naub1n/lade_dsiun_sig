354cd65dd8ce9e655d98324f88541f6a075b2536
Nicolas AUBIN <aubinnic@gmail.com>
2022-12-01 14:00:40 +0100

    Création des services Portainer et Traefik

--- os/templates/sig-os-post-intall.sh
+++ os/templates/sig-os-post-intall.sh
@@ -49,6 +49,9 @@ read DOCKER_USER
 # Déclaration de l'utilisateur dans loginctl (indispensable pour démarrer systemctl avec l'utilisateur)
 sudo loginctl enable-linger $DOCKER_USER
 
+# Exposer les ports < 1024
+sudo setcap cap_net_bind_service=ep $(which rootlesskit)
+
 # Installation de docker en mode rootless, démarrage du service et déclaration des variables d'environnement nécessaires
 # Cette partie doit se faire en ouvrant une session avec l'utilisateur concerné et ainsi démarrer une session loginctl.
 # Afin de ne pas lancer les commandes dans un autres script avec l'utilisateur souhaité, une connexion ssh locale est ouverte pour réaliser les commandes.
@@ -62,3 +65,4 @@ sudo ssh -o StrictHostKeyChecking=accept-new $DOCKER_USER@localhost \
     . ~/.profile && 
     docker context use rootless && 
     docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /$XDG_RUNTIME_DIR/docker.sock:/var/run/docker.sock -v ~/.local/share/docker/volumes:/var/lib/docker/volumes -v portainer_data:/data portainer/portainer-ce:latest'
+    docker run -d -p 8080:8080 -p 80:80 -v /app/traefik/traefik.yml:/etc/traefik/traefik.yml -v /$XDG_RUNTIME_DIR/docker.sock:/var/run/docker.sock traefik:v2.9
