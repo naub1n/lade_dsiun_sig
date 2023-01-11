614a603e52de13356a694b4daae6e8ba7a8362bd
Nicolas AUBIN <aubinnic@gmail.com>
2023-01-11 17:07:08 +0100

    Création dossier data docker

--- os/templates/sig-os-post-install-2.sh
+++ os/templates/sig-os-post-install-2.sh
@@ -4,6 +4,9 @@
 # Cette partie doit se faire en ouvrant une session avec l'utilisateur concerné et ainsi démarrer une session loginctl.
 # A lancer avec l'utilisateur 'maint' ou un utiliateur spécifique à l'application
 
+# Création du dossier pour les data docker. Attention le dossier /app doit être présent avec 'maint' comme propriétaire.
+mkdir -p /app/docker/data
+
 # Création du fichier de configuration de docker pour changer le chemin vers les data de Docker
 echo '{"data-root": "/app/docker/data"}' | python3 -m json.tool > ~/.config/docker/daemon.json
 
