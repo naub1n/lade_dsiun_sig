969d5c2861e778f6b108b22b7d6ac136d95ffdc3
Nicolas AUBIN <aubinnic@gmail.com>
2023-01-11 11:24:25 +0100

    Séparation de l'installation de Traefik et Portainer

---
+++ os/templates/sig-os-post-install-3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activation du contexte rootless pour Docker
+docker context use rootless
+
+# Création du sous réseau des application SIG (A modifier avec un nom générique si on souhaite faire de 
+# ce template un template pour l'ensemble du SI.
+docker network create sig-net
+
+# Téléchargement et déploiement de la stack Portainer
+wget -O portainer.yml https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/master/docker/standalone/portainer.yml
+docker compose -f portainer.yml -p portainer up -d
+
+# Préparation, téléchargement et déploiement de la stack Traefik
+mkdir -p /app/traefik/providers
+wget -O /app/traefik/providers/tls.yml https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/master/docker/standalone/traefik/tls.yml
+wget -O traefik.yml https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/master/docker/standalone/traefik/traefik.yml
+docker compose -f traefik.yml -p traefik up -d
