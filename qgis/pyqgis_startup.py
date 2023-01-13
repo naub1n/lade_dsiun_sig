ffcf4f0b423e6f867ad17b2a5befda9fb44af7f3
Nicolas AUBIN <aubinnic@gmail.com>
2023-01-13 09:00:39 +0100

    Version initiale pyqgis_startup.py

---
+++ qgis/pyqgis_startup.py
@@ -0,0 +1,17 @@
+
+# Script qui sera lancé au démarrage de QGIS 
+# Son chemin d'accès (github ou autre) est à déclarer dans la variable d'environnement PYQGIS_STARTUP
+
+import requests
+import os
+
+appdata_path = os.getenv('APPDATA')
+qgisdata_path = os.path.join(appdata_path, 'QGIS/QGIS3')
+
+script_url = "https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/developpement/qgis/startup.py"
+script_dest = os.path.join(qgisdata_path, 'startup.py')
+
+resp = requests.get(script_url, verify=False)
+
+with open(script_dest, "wb") as f:
+    f.write(resp.content)
