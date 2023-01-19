c17b2efacc44458f86768f5d9c9b9a3ebbb6022d
Nicolas AUBIN <aubinnic@gmail.com>
2023-01-19 16:25:35 +0100

    Modification de la procédure d'installation de portainer + https

--- os/templates/sig-os-post-install-3.sh
+++ os/templates/sig-os-post-install-3.sh
@@ -8,7 +8,9 @@ docker context use rootless
 docker network create sig-net
 
 # Téléchargement et déploiement de la stack Portainer
-wget -O portainer.yml https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/master/docker/standalone/portainer.yml
+wget -O portainer.yml https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/master/docker/standalone/portainer/portainer.yml
+echo "Indiquer l'alias complet à utiliser pour portainer. Ex: geoportainer.lesagencesdeleau.eu"
+read PORTAINER_HOST
 docker compose -f portainer.yml -p portainer up -d
 
 # Préparation, téléchargement et déploiement de la stack Traefik
