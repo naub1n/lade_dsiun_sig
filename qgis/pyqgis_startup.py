c3c38a2b2e854637f514ad4defddc962d3b74fc8
Nicolas AUBIN <aubinnic@gmail.com>
2023-05-04 15:44:35 +0200

    Ajout de logs pour le fichier pyqgis_startup.py

--- qgis/pyqgis_startup.py
+++ qgis/pyqgis_startup.py
@@ -5,13 +5,30 @@
 import requests
 import os
 
+from datetime import datetime
+
+def log(msg):
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S") + " - " + msg
+
+log_msg = []
+
 appdata_path = os.getenv('APPDATA')
 qgisdata_path = os.path.join(appdata_path, 'QGIS/QGIS3')
 
 script_url = "https://raw.githubusercontent.com/naub1n/lade_dsiun_sig/developpement/qgis/startup.py"
 script_dest = os.path.join(qgisdata_path, 'startup.py')
+log_dest = os.path.join(qgisdata_path, 'pyqgis_startup.log')
 
+log_msg.append("Lecture du script source startup.py")
 resp = requests.get(script_url, verify=False)
+log_msg.append("Code de la requête HTTP : %s" % resp.status_code)
 
+log_msg.append("Ecriture du fichier local startup.py")
 with open(script_dest, "wb") as f:
     f.write(resp.content)
+
+if not os.path.exists(log_dest):
+    log_msg.append("Erreur - Le fichier local startup.py n'a pas été trouvé après écriture")
+
+with open(log_dest, "w") as f:
+    f.writelines('\n'.join(log_msg))
